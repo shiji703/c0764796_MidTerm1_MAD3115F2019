504212abe7e4adefde3f113166e9fc4e0d84f9f8
shiji703 <shijieugin3@gmail.com>
2019-10-31 16:49:34 -0400

    customer view controller

---
+++ C0764796_MidTerm_MAD3115F2019/CustomerViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CustomerViewController.swift
+//  C0764796_MidTerm_MAD3115F2019
+//
+//  Created by MacStudent on 2019-10-31.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class CustomerViewController: NSObject {
+
+}
