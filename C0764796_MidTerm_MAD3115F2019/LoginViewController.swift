27f1dab084710b39b90491f150342538a6997e99
shiji703 <shijieugin3@gmail.com>
2019-10-31 15:47:03 -0400

    added sign in label

--- C0764796_MidTerm_MAD3115F2019/LoginViewController.swift
+++ C0764796_MidTerm_MAD3115F2019/LoginViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
