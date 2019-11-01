397e642d3728a6b24abd9e9406a624857be08a1f
bushair <bushairkm007@gmail.com>
2019-11-01 16:59:53 -0400

    changes

--- C0764796_MidTerm_MAD3115F2019/LoginViewController.swift
+++ C0764796_MidTerm_MAD3115F2019/LoginViewController.swift
@@ -38,7 +38,31 @@ class LoginViewController: UIViewController
                 self.present(nextVC, animated: true, completion: nil)
                 
     }
-  
+        else
+            
+        {
+            
+            let alert  =
+                
+                UIAlertController(title: "Error", message: "User Email / Password incorrect", preferredStyle: UIAlertController.Style.alert)
+            
+            //UIAlertController.Style.alert
+            
+            
+            
+            let actionOk = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+            
+            alert.addAction(actionOk)
+            
+            
+            
+            //Step - 4
+            
+            //Present Alert Dialog to the user
+            
+            self.present(alert, animated: true, completion: nil)
+            
+        }
   }
 
 }
